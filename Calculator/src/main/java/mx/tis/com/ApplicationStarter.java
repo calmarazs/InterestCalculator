f5bf83fcbeb83ce62eb65e476713bfc4c807a1cc
ALMARAZ SIERRA <cintiaalm.25@gmail.com>
2021-09-14 11:27:36 -0500

    Se agregaron dependencias cloud

--- Calculator/src/main/java/mx/tis/com/ApplicationStarter.java
+++ Calculator/src/main/java/mx/tis/com/ApplicationStarter.java
@@ -16,11 +16,13 @@ package mx.tis.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * The Class ApplicationStarter.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApplicationStarter {
   
   /**
